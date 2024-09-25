5c33c9e7e5dd7d4e771179573519b9729157e71f
VITHOR MARTINS SILVA <vithor.silva@alunos.sc.senac.br>
2024-09-25 16:57:45 -0300

    configurando axios api

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,38 @@
+import axios from "axios";
 import React from "react";
 import { useState } from "react";
 import Noodles from "../assets/noodle.jfif";
 
 const Serach = () => {
   const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+  const [error, setError] = useState("");
 
-  
-  const handleSubmit = (e) => {
+  const API_KEY = "";
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-  };
 
+    const url = "https://serpapi.com/search.json";
+    try {
+      const res = await axios.get(URL, {
+        params: {
+          q: query,
+          engine: "google",
+          google_domain: "google.com.br",
+          api_key: API_KEY,
+          hl: "pt-br",
+          gl: "br",
+          num: 10,
+        },
+      });
+      const data = await res.json();
+      setResults(data);
+    } catch (err) {
+      console.error(err);
+      setError("Ocorreu um erro ao fazer a busca");
+    }
+  };
 
   return (
     <div className="App">
