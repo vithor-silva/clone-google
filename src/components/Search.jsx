c83bb6fa0c8210f0c29daf73f5eeddf2f63d005d
VITHOR MARTINS SILVA <vithor.silva@alunos.sc.senac.br>
2024-09-25 16:22:00 -0300

    Criando hook para pesquisas

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import { useState } from "react";
 import Noodles from "../assets/noodle.jfif";
 
 const Serach = () => {
+  const [query, setQuery] = useState("");
+
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+
   return (
     <div className="App">
       <div className="Logo">
         <h1>Noodles</h1>
         <img src={Noodles} alt="Noodles"></img>
       </div>
-      <form>
-        <input type="text" placeholder="Sua busca" />
-        <button>Buscar</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Sua busca"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit">Buscar</button>
       </form>
     </div>
   );
